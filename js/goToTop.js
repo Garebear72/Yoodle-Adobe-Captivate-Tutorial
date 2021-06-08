08ebe70ea2b242d28d93092fafad3b79b716f4aa
Garry <78130426+Garebear72@users.noreply.github.com>
2021-06-08 05:09:56 -0500

    bug fixes w/ button + animations!

--- js/goToTop.js
+++ js/goToTop.js
@@ -15,27 +15,72 @@ function getmeasurements(){
     docheight = getDocHeight();
     trackLength = docheight - winheight;
 }
- 
+let x = true;
 function amountscrolled(){
     var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
     // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
     var pctScrolled = Math.floor(scrollTop/trackLength * 100);
     //console.log(pctScrolled + '% scrolled');
 
-    if (pctScrolled >= 3) {
-        $('#hovering-up-div-id').addClass('hovering-up-div');
-        $('#hovering-up-div-id').removeClass('hovering-down-div');
+    if (pctScrolled >= 5) {
+        if (x === true) {
+            $('.hovering-up-button').addClass('hovering-up-div');
+        }
+        
+        $('.hovering-up-button').removeClass('hovering-down-div');
         $('#hovering-up-div-id').removeClass('invisible');
     }
     else {
-        $('#hovering-up-div-id').removeClass('hovering-up-div');
-        $('#hovering-up-div-id').addClass('hovering-down-div');
-
+        x = true
+        $('.hovering-up-button').removeClass('hovering-up-div');
+        $('.hovering-up-button').addClass('hovering-down-div');
     }
 };
 
  
 getmeasurements();
+
+// function delay(ms) {
+//     $('.hovering-up-button').removeClass('shakeyshake');
+//     return new Promise(resolve => setTimeout(resolve, ms));
+    
+// }
+
+// delay(3000).then(() => $('.hovering-up-button').addClass('shakeyshake'));
+
+
+function test() {
+    var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    var pctScrolled = Math.floor(scrollTop/trackLength * 100);
+    let y = false;
+
+    if (pctScrolled >= 6) {
+        y = true;
+        $('.hovering-up-button').addClass('shakeyshake');
+        console.log('I\'m scrolled down enough, attempting to deal with a promise...');
+    }
+    else {
+        console.log('Not scrolled down far enough, trying again...')
+        y = false;
+        setTimeout(function() {test()}, 2000);
+    }
+
+    setTimeout(function() { 
+        if (y === true) {
+            x = false
+            $('.hovering-up-button').removeClass('shakeyshake');
+            $('.hovering-up-button').removeClass('hovering-up-div');
+            console.log('Function executed, waiting 20000ms.')
+            setTimeout(function() {test()}, 20000);
+        }
+    }, 1000);
+};
+
+test();
+
+
+
+
  
 window.addEventListener("resize", function(){
     getmeasurements();
@@ -55,12 +100,12 @@ $(document).ready(function() {
     // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
     var pctScrolled = Math.floor(scrollTop/trackLength * 100);
 
-    if (pctScrolled >= 3) {
-        $('#hovering-up-div-id').addClass('hovering-up-div');
-        $('#hovering-up-div-id').removeClass('hovering-down-div');
+    if (pctScrolled >= 5) {
+        $('.hovering-up-button').addClass('hovering-up-div');
+        $('.hovering-up-button').removeClass('hovering-down-div');
     }
 });
 
-$('#hovering-up-div-id').addClass('invisible');
+
 
 
