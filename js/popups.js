df2d94a47588c23fae9b5dc7e6cabd5339b51f75
Garry <78130426+Garebear72@users.noreply.github.com>
2021-06-07 22:15:39 -0500

    TONS of changes. Mostly FAQ edits, popups.

---
+++ js/popups.js
@@ -0,0 +1,82 @@
+tippy('#popper1', {
+  allowHTML: true,
+  placement: 'auto',
+  animation: 'perspective',
+  duration: 400,
+  content: '<img class="img-fluid" src="assets/images/faq1.png">',
+});
+
+tippy('#popper2', {
+  allowHTML: true,
+  maxWidth: 800,
+  placement: 'auto',
+  animation: 'perspective',
+  duration: 400,
+  content: '<img class="img-fluid" src="assets/images/faq2.gif">',
+})
+
+tippy('#popper3', {
+  allowHTML: true,
+  placement: 'auto',
+  animation: 'perspective',
+  duration: 400,
+  content: '<img class="img-fluid" src="assets/images/faq3.png">',
+});
+
+tippy('#popper4', {
+  allowHTML: true,
+  placement: 'auto',
+  animation: 'perspective',
+  duration: 400,
+  content: '<img class="img-fluid" src="assets/images/faq4.png">',
+});
+
+tippy('#popper5', {
+  allowHTML: true,
+  placement: 'auto',
+  animation: 'perspective',
+  duration: 400,
+  content: '<img class="img-fluid" src="assets/images/faq5.png">',
+});
+
+tippy('#popper6', {
+  allowHTML: true,
+  placement: 'auto',
+  animation: 'perspective',
+  duration: 400,
+  content: '<img class="img-fluid" src="assets/images/faq6.png">',
+});
+
+
+
+
+// $(document).ready(function(){
+    
+//     var popover1 = new bootstrap.Popover(document.querySelector('[title="Interactables"]'), {
+//         placement: 'auto',
+//         trigger: 'hover',
+//         html: true,
+//         content:'<img src="assets/images/faq1.png"/>'
+//       })
+
+//     var popover2 = new bootstrap.Popover(document.querySelector('[title="Timeline"]'), {
+//         placement: 'auto',
+//         trigger: 'click',
+//         html: true,
+//         container: '#faq',
+//         content:'<img class="img-fluid" src="assets/images/faq2.gif"/>',
+//       })
+
+//       popover1.enable();
+//       popover2.enable();
+      
+
+    // $('[title="Timeline"]').popover({
+    //     padding: 800,
+    //     placement: 'auto',
+    //     trigger: 'click',
+    //     html: true,
+    //     //container: '.container',
+    //     content:'<img class="img-fluid" src="assets/images/faq2.gif"/>',
+    // });
+// })
